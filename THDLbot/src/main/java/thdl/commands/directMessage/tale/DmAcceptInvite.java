808d7c95d04c550b18221df470afa6fb3e515853
Niklas <Niklas@P-PC.fritz.box>
2019-04-07 12:13:22 +0200

    DmAcceptInvite created

---
+++ THDLbot/src/main/java/thdl/commands/directMessage/tale/DmAcceptInvite.java
@@ -0,0 +1,38 @@
+package thdl.commands.directMessage.tale;
+
+import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
+import thdl.commands.directMessage.DirectCommand;
+
+
+public class DmAcceptInvite implements DirectCommand
+{
+
+	@Override
+	public boolean called(String[] args, PrivateMessageReceivedEvent event)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void action(String[] args, PrivateMessageReceivedEvent event) throws Exception
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void executed(boolean success, PrivateMessageReceivedEvent event)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String help()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
